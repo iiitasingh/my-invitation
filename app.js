8d65e4274a5f9e2eb18d05e6750f7d535ace1626
Ashish Singh <42268380+iiitasingh@users.noreply.github.com>
2019-11-23 00:05:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+require('express-async-errors');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+require('./mongo');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()).use(morgan());
+
+app.use("/invitations", require("./routes/invitationRoute"));
+app.use("/members", require("./routes/userRoutes"));
+
+app.use((req, res, next) => {
+    req.status = 404;
+    const error = new Error("Route not found");
+    next(error);
+})
+
+app.listen(port, () => { console.log("Connected") });
